7366ec0d4ca3d533597c1b8358152bb3c1ac9132
Tom Hunze <dev@thunze.de>
2025-12-08 17:22:33 +0100

    Set up project with basic Nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = nixpkgs.legacyPackages.${system};
+          }
+        );
+    in
+    {
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              bashInteractive
+            ];
+          };
+        }
+      );
+
+      formatter = forAllSystems ({ pkgs }: pkgs.nixfmt-tree);
+    };
+}
