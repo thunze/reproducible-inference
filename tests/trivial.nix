9934b43d659aac2f2d4c4685bd2910554de6affb
Tom Hunze <dev@thunze.de>
2026-01-09 20:34:42 +0100

    Implement trivial test using curl

--- tests/trivial.nix
+++ tests/trivial.nix
@@ -1,6 +1,8 @@
 {
   stdenv,
   llamaServerHook,
+  curl,
+  jq,
 }:
 
 stdenv.mkDerivation {
@@ -11,5 +13,27 @@ stdenv.mkDerivation {
 
   nativeBuildInputs = [
     llamaServerHook
+    curl
+    jq
   ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    mkdir -p $out
+
+    curl http://127.0.0.1:8080/v1/chat/completions \
+      -H "Content-Type: application/json" \
+      -d '{
+        "messages": [
+          {"role": "user", "content": "Hello, world!"}
+        ]
+      }' \
+      | jq -r '.choices.[0].message.content' \
+      > $out/response.txt
+    
+    cat $out/response.txt
+
+    runHook postBuild
+  '';
 }
