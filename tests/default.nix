c8ea1d13f680c35c1ac7bbd309ab47056f0ec2ec
Tom Hunze <dev@thunze.de>
2026-01-18 13:53:19 +0100

    Add test cases for CUDA

--- tests/default.nix
+++ tests/default.nix
@@ -1,21 +1,37 @@
 {
+  lib,
   callPackage,
   fetchurl,
 }:
 
 let
   writeTest =
-    module:
-    callPackage ../src/write-llama-wrapper.nix {
-      unwrapped = callPackage module { };
-      acceleration = false; # Use CPU for now
-      model = fetchurl {
-        url = "https://huggingface.co/ggml-org/gemma-3-270m-it-GGUF/resolve/main/gemma-3-270m-it-Q8_0.gguf";
-        hash = "sha256-DvV9LIOEWKGVJmQmDcujjlvdo3SU869zLwbkrdJAaOM=";
-      };
-    };
+    module: acceleration:
+    callPackage ../src/write-llama-wrapper.nix (
+      {
+        unwrapped = callPackage module { };
+        model = fetchurl {
+          url = "https://huggingface.co/ggml-org/gemma-3-270m-it-GGUF/resolve/main/gemma-3-270m-it-Q8_0.gguf";
+          hash = "sha256-DvV9LIOEWKGVJmQmDcujjlvdo3SU869zLwbkrdJAaOM=";
+        };
+        acceleration = if acceleration == "cpu" then false else acceleration;
+      }
+      // lib.optionalAttrs (acceleration == "cuda") {
+        # For our CUDA tests, we are using NVIDIA GPUs as old as the GeForce 900 series
+        # (compute capability 5.2), so we include architectures from 5.2 upwards.
+        # This will require compilation of llama-cpp with support for these architectures
+        # but gives us one binary that works across a wide range of GPUs.
+        cudaArchitecturesString = "52;60;61;70;75;80;86;89;90;100;120";
+      }
+    );
 in
 {
-  hello-curl = writeTest ./hello-curl;
-  hello-python = writeTest ./hello-python;
+  cpu = {
+    hello-curl = writeTest ./hello-curl "cpu";
+    hello-python = writeTest ./hello-python "cpu";
+  };
+  cuda = {
+    hello-curl = writeTest ./hello-curl "cuda";
+    hello-python = writeTest ./hello-python "cuda";
+  };
 }
