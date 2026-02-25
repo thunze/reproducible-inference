af62f72173203046927fc7be9532c3572567b537
Tom Hunze <dev@thunze.de>
2026-02-25 22:53:04 +0100

    Add `liveSystem` NixOS config and `liveSystemImage` package

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs, ... }:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -31,8 +31,33 @@
             inherit examples;
           };
           writeLlamaWrapper = pkgs.callPackage ./src/write-llama-wrapper.nix { };
+          liveSystemImage = self.nixosConfigurations.liveSystem.config.system.build.isoImage;
         }
       );
+
+      nixosConfigurations.liveSystem = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          (
+            { modulesPath, ... }:
+            {
+              imports = [
+                (modulesPath + "/installer/cd-dvd/installation-cd-graphical-gnome.nix")
+              ];
+              hardware.graphics.enable = true;
+
+              # Use the proprietary NVIDIA kernel module because we need to
+              # support GPU architectures older than Turing, which the open
+              # kernel module doesn't support.
+              hardware.nvidia.open = false;
+              services.xserver.videoDrivers = [ "nvidia" ];
+
+              # Faster squashfs compression, we don't care about file size
+              isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+            }
+          )
+        ];
+      };
 
       devShells = forAllSystems (
         { pkgs }:
