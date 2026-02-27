5231c9c8c3881d4d857395afb55a0c1528751a1b
Tom Hunze <dev@thunze.de>
2026-02-27 16:27:33 +0100

    Split `tests` package output into acceleration types and `all`

--- tests/default.nix
+++ tests/default.nix
@@ -13,17 +13,37 @@
 }:
 
 let
-  # Generate test cases in the expected format for all examples in `examples`.
+  # Generate test cases in the expected format for all available examples
+  # with acceleration type `acceleration`.
   # See `./runner.nix` for the expected format of each test case.
   # We stop recursing through `examples` when we encounter a derivation, which
   # should be the wrapped example script for a test case.
-  testCases = lib.mapAttrsToListRecursiveCond (path: attrset: !(lib.isDerivation attrset)) (
-    path: exampleDerivation: rec {
-      name = lib.concatStringsSep "_" path;
-      inherit exampleDerivation;
-      expectedOutputFile = ./expected/test_${name}_stdout.log;
-    }) examples;
+  generateTestCases =
+    acceleration:
+    lib.mapAttrsToListRecursiveCond (path: attrset: !(lib.isDerivation attrset)) (
+      path: exampleDerivation: rec {
+        name = "${acceleration}_${lib.concatStringsSep "_" path}";
+        inherit exampleDerivation;
+        expectedOutputFile = ./expected/test_${name}_stdout.log;
+      }) examples.${acceleration};
+
+  # Create a test runner for a list of test cases
+  createTestRunner = testCases: callPackage ./runner.nix { inherit testCases; };
+
+  # Map acceleration types to their corresponding test cases
+  testCasesByAcceleration = lib.genAttrs (lib.attrNames examples) generateTestCases;
 in
-callPackage ./runner.nix {
-  inherit testCases;
-}
+# The resulting attribute set looks like this:
+#
+# {
+#   cpu = <derivation of test runner for all `cpu` test cases>;
+#   cuda = <derivation of test runner for all `cuda` test cases>;
+#   ...
+#   all = <derivation of test runner for all test cases>;
+# }
+(lib.mapAttrs (acceleration: createTestRunner) (
+  testCasesByAcceleration
+  // {
+    all = lib.concatAttrValues testCasesByAcceleration;
+  }
+))
