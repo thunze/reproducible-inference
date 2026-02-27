2239423ce93e00f85ca90b698499f668edd0cdb3
Tom Hunze <dev@thunze.de>
2026-02-27 23:17:36 +0100

    Set `max_tokens` in `high-temperature` example

--- examples/high-temperature/default.nix
+++ examples/high-temperature/default.nix
@@ -13,6 +13,8 @@ writeShellApplication {
   ];
 
   # Disabling top-p and min-p sampling to isolate the effect of high temperature.
+  # Also setting an upper bound on the number of output tokens to prevent
+  # excessively long outputs that could result from the high temperature setting.
   #
   # We're not using the OpenAI Python client here because OpenAI-compatible APIs
   # don't officially support the `min_p` parameter.
@@ -25,7 +27,8 @@ writeShellApplication {
         ],
         "temperature": 10,
         "top_p": 1.0,
-        "min_p": 0.0
+        "min_p": 0.0,
+        "max_tokens": 500
       }' \
       | jq -r '.choices.[0].message.content'
   '';
